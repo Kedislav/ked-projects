c878ba11bf4d97e6fd9599d74dae2dee2126e27e
Ked <79175220+Kedislav@users.noreply.github.com>
2022-04-23 02:44:10 +0000

    Update Codecademy directory.

---
+++ Codecademy/Python3/initials.py
@@ -0,0 +1,9 @@
+# Hey, my name is Kedislav. I have never been able to sleep on my stomach.
+
+print('''
+K     K  E E E E  D D D
+K   K    E        D     D
+K K      E E E    D     D
+K   K    E        D     D
+K     K  E E E E  D D D 
+''')
